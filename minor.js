05ba01a031536695d77a6954b4df92323842c4b0
Conor <conorsg@gmail.com>
2014-03-11 11:47:54 -0500

    queue function to handle sequence of data calls

--- minor.js
+++ minor.js
@@ -1,6 +1,12 @@
+var c 			=	0;
+var fns 		=	[
+						getCrimeTypes,
+						getNewData,
+						getOldData
+					];
 var url 		= 	'https://data.nola.gov/resource/jsyu-nz5r.json?disposition=RTF&$select=typetext,timecreate,type_';
 var pastUrl 	= 	'http://data.nola.gov/resource/5fn8-vtui.json?disposition=RTF&$select=typetext,timecreate,type_';
-var typesUrl 	= 	"https://data.nola.gov/resource/jsyu-nz5r.json?disposition=RTF&$select=typetext&$group=typetext";
+var typesUrl 	= 	'https://data.nola.gov/resource/jsyu-nz5r.json?disposition=RTF&$select=typetext&$group=typetext';
 var crimeTypes 	=	[];
 var days		=	[];
 var cells		=	[];
@@ -9,50 +15,65 @@ var pastData;
 var nest;
 var pastNest;
 
+queue(fns);
+
+function queue(arr) {
+	arr[c]();
+	c++;
+}
 
 // retrieve 2014 crime types
-d3.json(typesUrl, function(error, json){
-	json.forEach(function(d){
-		crimeTypes.push(d.typetext);
+function getCrimeTypes() {
+	d3.json(typesUrl, function(error, json){
+		json.forEach(function(d){
+			crimeTypes.push(d.typetext);
+		});
+		queue(fns);
+		crimeTypes.done = true;
 	});
-	return crimeTypes.done = true;
-});
+}
 
 // retrieve 2014 data
-d3.json(url, function(error,json) {
-	data = json;
-	i = 1;
+function getNewData() {
+	d3.json(url, function(error,json) {
+		data = json;
+		i = 1;
 
-	(function pageData(json) {
-		if(json.length === 1000) {
-			d3.json(url + '&$offset=' + (i*1000), function(error,json) {
-				data = data.concat(json);
-				i++;
-				pageData(json);
-			});
-		} else {
-			data.done = true;
-		}
-	}) (json);	
-});
+		(function pageData(json) {
+			if(json.length === 1000) {
+				d3.json(url + '&$offset=' + (i*1000), function(error,json) {
+					data = data.concat(json);
+					i++;
+					pageData(json);
+				});
+			} else {
+				queue(fns);
+				data.done = true;
+			}
+		}) (json);	
+	});
+}
 
 // retrieve 2013 data
-d3.json(pastUrl, function(error,json) {
-	pastData = json;
-	i = 1;
+function getOldData() {
+	d3.json(pastUrl, function(error,json) {
+		pastData = json;
+		i = 1;
 
-	(function pageData(json) {
-		if(json.length === 1000) {
-			d3.json(pastUrl + '&$offset=' + (i*1000), function(error,json) {
-				pastData = pastData.concat(json);
-				i++;
-				pageData(json);
-			});
-		} else {
-			pastData.done = true;
-		}
-	}) (json);	
-});	
+		(function pageData(json) {
+			if(json.length === 1000) {
+				d3.json(pastUrl + '&$offset=' + (i*1000), function(error,json) {
+					pastData = pastData.concat(json);
+					i++;
+					pageData(json);
+				});
+			} else {
+				// queue(fns);
+				pastData.done = true;
+			}
+		}) (json);	
+	});	
+}
 
 function transform(data) {
 	data.forEach(function(d){
