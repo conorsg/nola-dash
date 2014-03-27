6cebe39872eccdbe8e1caf56302e06cb591a32d0
Conor <conorsg@gmail.com>
2014-03-27 17:05:10 -0500

    remove objects without dispatch time and convert the rest to time objects

--- 911.js
+++ 911.js
@@ -5,8 +5,9 @@ var fns = [];
 var url = 'https://data.nola.gov/resource/jsyu-nz5r.json?disposition=RTF&$select=timecreate,typetext,type_,timedispatch,timeclosed';
 var data;
 
+init();
 
-function dataCall() {
+function init() {
     d3.json(url, function(error,json) {
         data = json;
         i = 1;
@@ -19,8 +20,22 @@ function dataCall() {
                     pageData(json);
                 });
             } else {
+                transform(data); //call next fn in sequence
                 console.log(data.length)
             }
         }) (json);
+    });
+}
+
+function transform(data) {
+
+    data.forEach(function(d) {
+        if(!d.timedispatch) {
+            data.splice(data.indexOf(d), 1);
+        } else {
+            d.timecreate = new Date(d.timeclosed);
+            d.timedispatch = new Date(d.timedispatch);
+            d.timeclosed = new Date(d.timeclosed);
+        }
     });
 }
