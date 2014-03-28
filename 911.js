019fe716118ecc8c4c995a380da8fc1d0169c512
Conor <conorsg@gmail.com>
2014-03-28 16:46:18 -0500

    markup for charts, data objects for nvd3 charts

--- 911.js
+++ 911.js
@@ -1,10 +1,13 @@
-// Distribution of 911 response times
+// Distribution of 911 call response times
 
 var c = 0;
 var fns = [];
+var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 var url = 'https://data.nola.gov/resource/jsyu-nz5r.json?disposition=RTF&$select=timecreate,typetext,type_,timedispatch,timeclosed';
 var data;
 var responses = [];
+var freqDate = [];
+var freqTime = [];
 
 init();
 
@@ -33,9 +36,31 @@ function timify(data) {
             d.timecreate = new Date(d.timecreate);
             d.timedispatch = new Date(d.timedispatch);
             d.timeclosed = new Date(d.timeclosed);
+
             d.timeresponse = (d.timedispatch - d.timecreate);
+            d.timeint = 0.5 * Math.round( (d.timeresponse/6000)/0.5 ) // rounded to the nearest half minute
 
             responses.push(d);
         }
     });
+    makeChartData();
+}
+
+function makeChartData() {
+    responses.forEach(function(r) {
+        //data points for response time frequencies by date
+        freqDate.push({
+            key: months[r.timecreate.getMonth()] + " " + r.timecreate.getDate().toString(),
+            value: r.timeresponse
+        });
+    });
+
+    freqTime = d3.nest()
+                    .key(function(d) { return d.timeint })
+                    .entries(responses)
+
+
+    for(f in freqTime) {
+        freqTime[f].values = freqTime[f].values.length
+    }
 }
